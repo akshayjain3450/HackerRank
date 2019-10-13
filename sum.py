c8d2d555d032c192db6b7720fcd78bd4fc91de89
Kapil <kapil@Kapil>
2019-10-14 02:59:34 +0530

    python code for sum and diff of int and float

---
+++ sum.py
@@ -0,0 +1,13 @@
+# Store input numbers	    
+num1, num2 = input("").split()
+num3, num4 = input("").split()
+# Add two numbers
+sum1 = int(num1) + int(num2)
+sum2 = float(num3) + float(num4)
+# Subtract two number
+sub1 = int(num1) - int(num2)
+sub2 = float(num3) - float(num4)
+# Displa the sum(int)
+print(sum1,sub1)
+# Displa the difference(float)
+print(sum2,sub2)
