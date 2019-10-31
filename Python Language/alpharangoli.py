26b719e6e12077fa263eebe771a9256ffd07748e
akshayjain3450 <akshayjain3450@gmail.com>
2019-10-31 12:43:34 +0530

    Alphabet Rangoli

---
+++ Python Language/alpharangoli.py
@@ -0,0 +1,17 @@
+import string
+
+def print_rangoli(size):
+    N = size
+    mid = N - 1
+    for i in range(N - 1, 0, -1):
+        row = ['-'] * (2 * N - 1)
+        for j in range(N - i):
+            row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+        print('-'.join(row))
+    for i in range(0, N):
+        row = ['-'] * (2 * N - 1)
+        for j in range(0, N - i):
+            row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+        print('-'.join(row))
+
+if __name__ == '__main__':
